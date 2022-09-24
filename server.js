1f1d5ef14f6331e41226ae79ecaac7123eee9b0a
Naveen Krish <98112231+Naveen-McDev@users.noreply.github.com>
2022-09-24 18:10:37 +0530

    login and registration backend

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const dbConnect = require("./config/dbConnect");
+const userRoute = require("./routes/userRoute");
 dbConnect();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/user", userRoute);
+
+
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
